8863305bbf0f0af6f06a2c8185aaf3939c5a849a
Ashok Kumar Kottagundu <akottagundu@tollplus.com>
2021-01-14 16:48:03 +0530

    Code modified as per the new requirement.

--- GitIntigrationWithVS/Controllers/HomeController.cs
+++ GitIntigrationWithVS/Controllers/HomeController.cs
@@ -20,11 +20,13 @@ namespace GitIntigrationWithVS.Controllers
 
         public IActionResult Index()
         {
+            ViewBag.Message = "This code has been added by Ashok inside Ashok branch";
             return View();
         }
 
         public IActionResult Privacy()
         {
+            ViewBag.Message = "Privacy() action method has been modified by Ashok inside Ashok branch";
             return View();
         }
 
