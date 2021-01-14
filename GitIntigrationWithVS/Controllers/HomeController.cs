2b9d6de39c8909b510146d09fb179db45534635c
Ashok Kumar Kottagundu <akottagundu@tollplus.com>
2021-01-14 16:54:51 +0530

    committed

--- GitIntigrationWithVS/Controllers/HomeController.cs
+++ GitIntigrationWithVS/Controllers/HomeController.cs
@@ -20,13 +20,11 @@ namespace GitIntigrationWithVS.Controllers
 
         public IActionResult Index()
         {
-            ViewBag.Message = "This code has been added by Ashok inside Ashok branch";
             return View();
         }
 
         public IActionResult Privacy()
         {
-            ViewBag.Message = "Privacy() action method has been modified by Ashok inside Ashok branch";
             return View();
         }
 
